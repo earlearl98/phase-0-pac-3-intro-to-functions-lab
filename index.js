5f1b1111ae9ecaef3b6ae2f39c075b47e715de13
earlearl98 <earlearl1720@gmail.com>
2023-09-14 04:17:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -17,5 +17,31 @@ function logWhisper(string) {
 
 
 function sayHiToHeadphonedRoommate(string) {
-  expect (string).toEqual (string);
+  // Check if the string is all lowercase
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  }
+  
+  // Check if the string is all uppercase
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  }
+  
+  // Check if the string is equal to "Let's have dinner together!"
+  if (string === "Let's have dinner together!") {
+    return "I would love to!";
+  }
+
+  // If none of the conditions above are met, you can provide a default response.
+  // For example, you could return something like "What did you say?"
+  
+  // Alternatively, you could return an error message if needed.
+  
+  // Make sure to handle all possible cases based on the requirements.
 }
+
+// Example usages:
+console.log(sayHiToHeadphonedRoommate('hello')); // "I can't hear you!"
+console.log(sayHiToHeadphonedRoommate('HELLO')); // "YES INDEED!"
+console.log(sayHiToHeadphonedRoommate("Let's have dinner together!")); // "I would love to!"
+
